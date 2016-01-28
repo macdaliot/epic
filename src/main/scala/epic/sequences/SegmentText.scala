1a807f1d55c43d8267ae6c9ded6286093b9a7a57
gowemee <milica@recordedfuture.com>
2016-01-28 10:48:59 +0100

    test

--- src/main/scala/epic/sequences/SegmentText.scala
+++ src/main/scala/epic/sequences/SegmentText.scala
@@ -6,9 +6,9 @@ import epic.util.ProcessTextMain
 /**
  * Simple class that reads in a bunch of files and parses them. Output is dumped to standard out.
  * @author dlwh
+  *         pushtest
  */
 object SegmentText extends ProcessTextMain[SemiCRF[Any, String], Segmentation[Any, String]] {
-
 
   override def render(model: SemiCRF[Any, String], ann: Segmentation[Any, String], tokens: IndexedSeq[String]): String = {
     ann.render
