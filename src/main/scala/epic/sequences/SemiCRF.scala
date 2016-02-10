a99873a000c4a78c45c437f513b449522519ca1c
elinromare <elin.romare@gmail.com>
2016-02-10 13:57:57 +0100

    Comment

--- src/main/scala/epic/sequences/SemiCRF.scala
+++ src/main/scala/epic/sequences/SemiCRF.scala
@@ -634,7 +634,7 @@ object SemiCRF {
           while (prevLabel < numLabels) {
             val prevScore = forwardScores(begin)(prevLabel)
             if (prevScore != 0.0) {
-              val score = m.transitionMarginal(prevLabel, label, begin, end) + prevScore
+              val score = m.transitionMarginal(prevLabel, label, begin, end) + prevScore //SHould not be added, but multiplied for CRF
               if (score > forwardScores(end)(label)) { // He makes numLabels^2 calc. for each label, ignores previous seq
                 forwardScores(end)(label) = score
                 forwardLabelPointers(end)(label) = prevLabel
