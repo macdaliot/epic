924452a89450459660bb1133db6fff7f39943bb7
gowemee <milica@recordedfuture.com>
2016-02-16 11:17:56 +0100

    Python script updated opefully tester calls python correctly

--- src/main/scala/JavaProject/PythonScripts/writeFilesFromDatabase.py
+++ src/main/scala/JavaProject/PythonScripts/writeFilesFromDatabase.py
@@ -2,7 +2,7 @@ import pymongo
 import sys
 import os
 from pymongo import MongoClient
-#from makeConllFromDBOutput import makeConll
+from makeConllFromDBOutput import makeConll
 #from getJustSentences import getJustSentences
 
 # python will convert \n to os.linesep
@@ -37,54 +37,54 @@ unlabeledPool.remove({})
 #db.create_collection("malware_training")
 #testMalware = db.malware_test
 #bigPool = db.malware_training
-# trainingCollection = allMalware.find({"random" : { "$gt": 0, "$lt": sizeOfTraining }})
-# testCollection = allMalware.find({"random" : { "$gt": sizeOfTraining, "$lt": endSet }})
-# bigPool.insert(trainingCollection)
-# testMalware.insert(testCollection)
-#
-#
-#
-# ########################
-# # Divide the training set into a small labeled pool for the model to start on
-# # and a bigger unlabeled pool which we will train the model with.
-# sizeOfstartPool = 0.05
-# tmpStartPool = bigPool.find({"random" : { "$gt": 0, "$lt": sizeOfstartPool}})
-# tmpUnlabeledPool = bigPool.find({"random" : { "$gt": sizeOfstartPool, "$lt": 1 }})
-# startPool = db.malware_labeled
-# unlabeledPool = db.malware_unlabeled
-# startPool.insert(tmpStartPool)
-# unlabeledPool.insert(tmpUnlabeledPool)
+trainingCollection = allMalware.find({"random" : { "$gt": 0, "$lt": sizeOfTraining }})
+testCollection = allMalware.find({"random" : { "$gt": sizeOfTraining, "$lt": endSet }})
+bigPool.insert(trainingCollection)
+testMalware.insert(testCollection)
+
+
+
+########################
+# Divide the training set into a small labeled pool for the model to start on
+# and a bigger unlabeled pool which we will train the model with.
+sizeOfstartPool = 0.05
+tmpStartPool = bigPool.find({"random" : { "$gt": 0, "$lt": sizeOfstartPool}})
+tmpUnlabeledPool = bigPool.find({"random" : { "$gt": sizeOfstartPool, "$lt": 1 }})
+startPool = db.malware_labeled
+unlabeledPool = db.malware_unlabeled
+startPool.insert(tmpStartPool)
+unlabeledPool.insert(tmpUnlabeledPool)
 
 
 ############################
 # Create a labeledPool and an unlabeledPool file to which we write the data of the labeled pool collection
 # Make this text file into a conll file using makeConll. 
 # # This will later be used to train epic.
-# tmpStartPool = startPool.find({"random" : { "$gt": 0, "$lt": 1}})
-# f = open(os.path.expanduser("~/epic/epic/data/labeledPool.txt"),'w')
-# #f = open("labeledPool.txt",'w')
-# #stringarray = []
-# for i in range(1,tmpStartPool.count()):
-# 	f.write(str(tmpStartPool[i]))
-# 	#	stringarray.append(tmpStartPool[i])
-# 	f.write("\n")
-#
-# f.close()
-# tmpUnlabeledPool = unlabeledPool.find({"random" : { "$gt": 0, "$lt": 1}})
-# f = open(os.path.expanduser("~/epic/epic/data/unlabeledPool.txt"),'w')
-# #f = open("unlabeledPool.txt",'w')
-# for i in range(1,tmpUnlabeledPool.count()):
-# 	f.write(str(tmpUnlabeledPool[i]))
-# 	f.write("\n")
-#
-# f.close()
+tmpStartPool = startPool.find({"random" : { "$gt": 0, "$lt": 1}})
+f = open(os.path.expanduser("~/epic/epic/data/labeledPool.txt"),'w')
+#f = open("labeledPool.txt",'w')
+#stringarray = []
+for i in range(1,tmpStartPool.count()):
+	f.write(str(tmpStartPool[i]))
+	#	stringarray.append(tmpStartPool[i])
+	f.write("\n")
+
+f.close()
+tmpUnlabeledPool = unlabeledPool.find({"random" : { "$gt": 0, "$lt": 1}})
+f = open(os.path.expanduser("~/epic/epic/data/unlabeledPool.txt"),'w')
+#f = open("unlabeledPool.txt",'w')
+for i in range(1,tmpUnlabeledPool.count()):
+	f.write(str(tmpUnlabeledPool[i]))
+	f.write("\n")
+
+f.close()
 
 
 
-#tmp_file = open(os.path.expanduser('~/epic/epic/data/labeledPool.conll'))
-#tmp_file.close()
+tmp_file = open(os.path.expanduser('~/epic/epic/data/labeledPool.conll'))
+tmp_file.close()
 
-#makeConll('~/epic/epic/data/labeledPool.txt', '~/epic/epic/data/labeledPool.conll')
+makeConll('~/epic/epic/data/labeledPool.txt', '~/epic/epic/data/labeledPool.conll')
 
 print "poop"
 
