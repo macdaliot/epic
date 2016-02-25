677645640dbb45f4ce9082606b75fcfbddd70a5d
gowemee <milica@recordedfuture.com>
2016-02-25 09:08:35 +0100

    Sisters fiddled some more

--- src/main/scala/JavaProject/Tester.java
+++ src/main/scala/JavaProject/Tester.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.lang.*;
+import java.nio.channels.FileChannel;
 
 import epic.sequences.SemiCRF;
 import epic.sequences.SemiCRF$;
@@ -20,31 +21,35 @@ public class Tester {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
         File fileNameWordFreq = new File("/Users/" + args[0] + "/Dropbox/Exjobb/PythonThings/wordFreq.txt");
-        String s = null;
         double noiseParameter = 1;
+        String s = null;
+        String[] trainingString = {"--train",
+                "data/labeledPool.conll",
+                "--test", "data/conllFileTest.conll",
+                "--modelOut", "data/our_malware.ser.gz"};
         // The name of the file to open
+
+        copyFile(args[0]); //Copys sets to txt files
+
         File fileNameUnlabeledSet = new File("/Users/" + args[0] + "/epic/epic/data/unlabeledPool.txt");
         File fileNameLabeledSet = new File("/Users/" + args[0] + "/epic/epic/data/labeledPool.txt");
+        SelectQuery sq = new SelectQuery();
+        SelectQueryRandom sqr = new SelectQueryRandom();
         String modelFileName = "./data/our_malware.ser.gz";
         System.out.println("Welcome " + args[0]);
         System.out.println("Sit down and let me work my magic");
         CreatePythonFile cp = new CreatePythonFile();
         List<String> sentences;
         Batch b;
+        int batchSize = 10;
+        if (args.length>1){
+            batchSize = Integer.parseInt(args[1]);
+        }
         PrintWriter writer;
         double noise = 0.15/(4000/274);
         int totalPoolSize = 0;
         try {
             writer = new PrintWriter("/Users/" + args[0] + "/epic/epic/data/unsure.txt", "UTF-8");
-
-            int batchSize = 10;
-
-            if (args.length>1){
-                batchSize = Integer.parseInt(args[1]);
-            }
-
-            SelectQuery sq = new SelectQuery();
-            SelectQueryRandom sqr = new SelectQueryRandom();
             int modelChoice = 1;
             List<Double> batch = new ArrayList<Double>();
             batch.add(0.0);
@@ -55,49 +60,13 @@ public class Tester {
                 pw.close();
             } catch(IOException fe){ System.out.println(fe);}
 
-            String[] trainingString = {"--train",
-                    "data/labeledPool.conll",
-                    "--test", "data/conllFileTest.conll",
-                    "--modelOut", "data/our_malware.ser.gz"};
             if (Integer.parseInt(args[2]) == 1 ) {
-                try {
-                    Process p = Runtime.getRuntime().exec("python src/main/scala/JavaProject/PythonScripts/writeFilesFromDatabase.py 0.8 1 "+Double.toString(noise));
-                    BufferedReader stdInput = new BufferedReader(new
-                            InputStreamReader(p.getInputStream()));
-                    BufferedReader stdError = new BufferedReader(new
-                            InputStreamReader(p.getErrorStream()));
-                    // read the output from the command
-                    System.out.println("Here is the standard output of the command writeFiles:\n");
-                    while ((s = stdInput.readLine()) != null) {
-                        System.out.println(s);
-                    }
-                } catch (IOException ex) {
-                    System.out.println(
-                            "Something went wrong when getRunTime on first training: " + ex);
-                }
+                splitAndWriteDB(noise);
                 SemiConllNerPipeline.main(trainingString);
                 System.out.println("Finished training first model");
 
             }
-            try{
-                FileReader tmpL = new FileReader(fileNameLabeledSet);
-            FileReader tmpUn = new FileReader(fileNameUnlabeledSet);
-            BufferedReader tmpl = new BufferedReader(tmpL);
-            BufferedReader tmpun = new BufferedReader(tmpUn);
-            while ((tmpl.readLine()) != null) {
-                totalPoolSize++;
-            }
-            while ((tmpun.readLine()) != null) {
-                totalPoolSize++;
-            }
-            System.out.println("Total pool size is "+ totalPoolSize);
-            System.out.println("Finished writing from database");
-        } catch (IOException ex) {
-            System.out.println(
-                    "Something went wrong when getRunTime on first training: " + ex);
-        }
-
-
+            totalPoolSize = getPoolSize(fileNameLabeledSet, fileNameUnlabeledSet);
 
             boolean labelNewbatch = true;
 
@@ -150,23 +119,7 @@ public class Tester {
                     if (batch.size() == 0) {
                         break;
                     }
-                    cp.CreatePythonFile(batch, noise);
-                    try {
-                        Process p = Runtime.getRuntime().exec("python src/main/scala/JavaProject/PythonScripts/tmp.py");
-                        BufferedReader stdInput = new BufferedReader(new
-                                InputStreamReader(p.getInputStream()));
-                        BufferedReader stdError = new BufferedReader(new
-                                InputStreamReader(p.getErrorStream()));
-                        // read the output from the command
-                        System.out.println("Here is the standard output of the command MoveBatch:\n");
-                        while ((s = stdInput.readLine()) != null) {
-                            System.out.println(s);
-                        }
-                    } catch (IOException ex) {
-                        System.out.println(
-                                "Something went wrong when getRunTime of tmp");
-                    }
-
+                    moveBatch(cp,noise,batch);
                     SemiConllNerPipeline.main(trainingString);
                     if (c == 1000){
                         labelNewbatch = false;
@@ -177,42 +130,14 @@ public class Tester {
                     SemiCRF<String, String> model = getModel.getModel(modelFileName);
                     //batch = sqr.SelectQueryRandom(fileNamelabeledSet, batchSize);
                     b = sq.SelectQuery(fileNameLabeledSet, 200, modelChoice, model);
-                    sentences = b.getSentences();
                     labelNewbatch = true;
-                    batch = b.getIds();
-                    int medSentLength = 0;
-                    String medLC = "";
-                    for (int i = 0; i < batch.size(); i++) {
-                        String tmp = sentences.get(i).replace(". . \n","");
-                        tmp = tmp.replace(". . B_MALWARE\n","");
-                        tmp = tmp.replace(". . I_MALWARE\n","");
-                        //System.out.println(tmp);
-                        String[] splitSentence = tmp.split(" ");
-                        medSentLength += (splitSentence.length-1)/batchSize;
-                        medLC += Double.parseDouble(splitSentence[0])/batchSize;
-                        writer.println(batch.get(i) + " " +sentences.get(i));
-                    }
-                    writer.println("Medium LC value: " + medLC+" Medium sent length: " + medSentLength);
+                    writeUnsure(b,batchSize, writer);
 
                     //if (batch.size() == 0) {
-                        break;
+                    break;
                     //}
-                    /*cp.CreatePythonFile(batch, noise);
-                    try {
-                        Process p = Runtime.getRuntime().exec("python src/main/scala/JavaProject/PythonScripts/tmp.py");
-                        BufferedReader stdInput = new BufferedReader(new
-                                InputStreamReader(p.getInputStream()));
-                        BufferedReader stdError = new BufferedReader(new
-                                InputStreamReader(p.getErrorStream()));
-                        // read the output from the command
-                        System.out.println("Here is the standard output of the command MoveBatch:\n");
-                        while ((s = stdInput.readLine()) != null) {
-                            System.out.println(s);
-                        }
-                    } catch (IOException ex) {
-                        System.out.println(
-                                "Something went wrong when getRunTime of tmp");
-                    }
+                    /*
+                    moveBatch(cp,noise,batch)
 
                     SemiConllNerPipeline.main(trainingString);*/
                 }
@@ -232,6 +157,128 @@ public class Tester {
         }
     }
 
-    // java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester urName
+    private static void copyFile(String user){
+        File sourceFile1 = new File("/Users/" + user + "/epic/epic/data/unlabeledPoolStart.txt");
+        File destFile1 = new File("/Users/" + user + "/epic/epic/data/unlabeledPool.txt");
+        File sourceFile2 = new File("/Users/" + user + "/epic/epic/data/labeledPoolStart.txt");
+        File destFile2 = new File("/Users/" + user + "/epic/epic/data/labeledPool.txt");
 
+        FileChannel source1 = null;
+        FileChannel destination1 = null;
+        FileChannel source2 = null;
+        FileChannel destination2 = null;
+        try{
+            try {
+                source1 = new FileInputStream(sourceFile1).getChannel();
+                destination1 = new FileOutputStream(destFile1).getChannel();
+                destination1.transferFrom(source1, 0, source1.size());
+                source2 = new FileInputStream(sourceFile2).getChannel();
+                destination2 = new FileOutputStream(destFile2).getChannel();
+                destination2.transferFrom(source2, 0, source2.size());
+            }
+            finally {
+                if(source1 != null) {
+                    source1.close();
+                }
+                if(destination1 != null) {
+                    destination1.close();
+                }
+                if(source2 != null) {
+                    source2.close();
+                }
+                if(destination2 != null) {
+                    destination2.close();
+                }
+            }
+        }
+        catch (IOException ex) {
+            System.out.println(
+                    "Something went wrong when getRunTime on first training: " + ex);
+        }
+
+
+    }
+    // java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester urName
+    private static void splitAndWriteDB(double noise){
+        String s = null;
+        try {
+            Process p = Runtime.getRuntime().exec("python src/main/scala/JavaProject/PythonScripts/writeFilesFromDatabase.py 0.8 1 "+Double.toString(noise));
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(p.getErrorStream()));
+            // read the output from the command
+            System.out.println("Here is the standard output of the command writeFiles:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (IOException ex) {
+            System.out.println(
+                    "Something went wrong when getRunTime on first training: " + ex);
+        }
+
+    }
+
+    private static int getPoolSize(File fileNameLabeledSet, File fileNameUnlabeledSet){
+        int size = 0;
+        try{
+            FileReader tmpL = new FileReader(fileNameLabeledSet);
+            FileReader tmpUn = new FileReader(fileNameUnlabeledSet);
+            BufferedReader tmpl = new BufferedReader(tmpL);
+            BufferedReader tmpun = new BufferedReader(tmpUn);
+            while ((tmpl.readLine()) != null) {
+                size++;
+            }
+            while ((tmpun.readLine()) != null) {
+                size++;
+            }
+            System.out.println("Total pool size is "+ size);
+            System.out.println("Finished writing from database");
+        } catch (IOException ex) {
+            System.out.println(
+                    "Something went wrong when getRunTime on first training: " + ex);
+        }
+        return size;
+
+    }
+
+    private static void moveBatch(CreatePythonFile cp, double noise,List<Double> batch){
+        cp.CreatePythonFile(batch, noise);
+        String s=null;
+        try {
+            Process p = Runtime.getRuntime().exec("python src/main/scala/JavaProject/PythonScripts/tmp.py");
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(p.getErrorStream()));
+            // read the output from the command
+            System.out.println("Here is the standard output of the command MoveBatch:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (IOException ex) {
+            System.out.println(
+                    "Something went wrong when getRunTime of tmp");
+        }
+
+    }
+
+    private static void writeUnsure(Batch b, int batchSize,PrintWriter writer){
+
+        List<Double>batch = b.getIds();
+        List<String>sentences = b.getSentences();
+        int medSentLength = 0;
+        String medLC = "";
+        for (int i = 0; i < batch.size(); i++) {
+            String tmp = sentences.get(i).replace(". . \n","");
+            tmp = tmp.replace(". . B_MALWARE\n","");
+            tmp = tmp.replace(". . I_MALWARE\n","");
+            //System.out.println(tmp);
+            String[] splitSentence = tmp.split(" ");
+            medSentLength += (splitSentence.length-1)/batchSize;
+            medLC += Double.parseDouble(splitSentence[0])/batchSize;
+            writer.println(batch.get(i) + " " +sentences.get(i));
+        }
+        writer.println("Medium LC value: " + medLC+" Medium sent length: " + medSentLength);
+    }
 }
