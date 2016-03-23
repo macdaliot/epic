c86bbf8aa899cec0f90727191044926fe3faf107
elinromare <elin.romare@gmail.com>
2016-03-23 14:27:55 +0100

    EvalNegAdditions added and started

--- src/main/scala/evaluation/EvaluateNegativeAdditions.scala
+++ src/main/scala/evaluation/EvaluateNegativeAdditions.scala
@@ -2,12 +2,12 @@ package evaluation
 
 import java.io.File
 
-import epic.sequences.{SemiCRF, SemiConllNerPipeline}
+import epic.sequences.SemiCRF
+import epic.sequences.SemiConllNerPipeline
 
 /**
   * Created by elin on 23/03/16.
   */
-public
 
 object EvaluateNegativeAdditions {
 
@@ -15,17 +15,31 @@ object EvaluateNegativeAdditions {
     val modelFileName: String = "./data/our_malware.ser.gz"
     val modelFile: File = new File(modelFileName)
     val model: SemiCRF[String, String] = breeze.util.readObject(modelFile)
-    val testFileName:  String = ""
+    val testFileName:  String = "./data/epicEvalutationTestSet"
+    val posFileName: String = "./data/positiveTestSet"
+    val negFileName: String = "./data/negativeTestSet"
+    val fakePosFileName: String = "./data/fakePositiveTestSet"
     val testFile: File = new File(testFileName)
+    val posFile: File = new File(posFileName)
+    val negFile: File = new File(negFileName)
+    val fakePosFile: File = new File(fakePosFileName)
     val folderOfData: String = ""
     var counter: Int = 0
     for (file <- new File("./data/EvaluateNegativeAdditionsDataSets").listFiles) {
       counter +=1
       val trainingString: Array[String] = Array("--train", "data/EvaluateNegativeAdditionsDataSets/dataSet" + counter + ".conll",
       "--test", "data/epicEvalutationTestSet.conll", "--modelOut", modelFileName,
-        "--useStochastic", "false", "--regularization", "1");
+        "--useStochastic", "false", "--regularization", "1")
       SemiConllNerPipeline.main(trainingString)
-      EpicEvaluation.evaluateModel(File, model)
+      val statsPos: Array[Double] = EpicEvaluation.evaluateModel(posFile, model)
+      val statsNeg: Array[Double] = EpicEvaluation.evaluateModel(negFile, model)
+      val statsFakePos: Array[Double] = EpicEvaluation.evaluateModel(fakePosFile, model)
+      val stats: Array[Double] = EpicEvaluation.evaluateModel(testFile, model)
+
+      println("Stats: " + stats.mkString(" "))
+      println("Stats pos: " + statsPos.mkString(" "))
+      println("Stats neg: " + statsNeg.mkString(" "))
+      println("Stats fake pos: " + statsFakePos.mkString(" "))
     }
   }
 }
