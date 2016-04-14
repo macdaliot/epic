7edd38828bfac86f90929886480800200df6248c
gowemee <milica@recordedfuture.com>
2016-04-14 14:59:29 +0200

    Gibbs run

--- batchTest.sh
+++ batchTest.sh
@@ -2,5 +2,5 @@
 
 for i in 3 5 7 10 15
 do
-   java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 vote $i train
+   java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 gibbs train
 done
