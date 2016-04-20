9e156b9e4fd2e9376a2fc15fa877756ce316ada2
gowemee <milica@recordedfuture.com>
2016-04-20 14:30:04 +0200

    Gibbs run

--- batchTest.sh
+++ batchTest.sh
@@ -6,13 +6,6 @@ do
 done
 
 
-#Helgen
-#java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 lc train
-#java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 infodens train
-#java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 random train
-#java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 gibbs train
-#java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 vote 3 train
-
 
 
 #for i in $(seq 100 100 600)
@@ -20,5 +13,9 @@ done
 #   java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester thresh 300 train
 #done
 
+#Helgen
 #java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 lc infodens train
+#java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 vote 3 infodens train
+#java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 gibbs infodens train
+
 #java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 lc noise train
