75f8a255dbe9f12149a96d3fa5cac9878584951c
gowemee <milica@recordedfuture.com>
2016-04-22 10:36:19 +0200

    Threshrun

--- batchTest.sh
+++ batchTest.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-for i in 3 5 7 10
-do
-   java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 vote $i train
-done
-
-
-
-
-#for i in $(seq 100 100 600)
+#for i in 3 5 7 10
 #do
-#   java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester thresh 300 train
+#   java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 vote $i train
 #done
+
+
+
+
+for i in $(seq 100 100 600)
+do
+   java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester thresh $i train
+done
 
 #Helgen
 #java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 lc infodens train
