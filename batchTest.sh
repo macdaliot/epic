1801ef42d8d6f948afb124137fdddf3dc2f9eecf
gowemee <milica@recordedfuture.com>
2016-04-22 14:53:34 +0200

    Gibbs run

--- batchTest.sh
+++ batchTest.sh
@@ -15,8 +15,14 @@ done
 
 #Helgen
 #java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 lc infodens train
-#java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 vote 3 infodens train
+#java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 vote 5 infodens train
 #java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 gibbs infodens train
+
+
+#for i in 0.25 0.5 1 2 4
+#do
+#   java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 lc infodens $i train
+#done
 
 
 #java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 lc noise train
