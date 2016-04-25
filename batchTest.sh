943145b589eea65a7355e7b94e9ccef885f1b051
gowemee <milica@recordedfuture.com>
2016-04-25 14:16:00 +0200

    Gibbs run

--- batchTest.sh
+++ batchTest.sh
@@ -13,13 +13,8 @@
 #   java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester threshold $i train
 #done
 
-#Helgen
-java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 lc infodens train
-java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 vote 5 infodens train
-java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 gibbs infodens train
 
-
-for i in 0.25 0.5 1 2 4
+for i in 0 0.25 0.5 1 2 4 100
 do
    java -cp target/scala-2.11/epic-assembly-0.4-SNAPSHOT.jar JavaProject.Tester 600 lc infodens $i train
 done
